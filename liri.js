75962fc5168e4a053baf71a53b365a1159f008d0
Eric Valdivia <ericv0801@gmail.com>
2020-02-04 08:19:19 -0500

    fixed do what it says function

--- liri.js
+++ liri.js
@@ -1,106 +1,165 @@
+// DEPENDENCIES
+// =====================================
+// Read and set environment variables
 require("dotenv").config();
-var moment = require('moment');
-var fs=require("fs")
-var omdb = require('omdb');
+
+// Import the node-spotify-api NPM package.
+var Spotify = require("node-spotify-api");
+
+// Import the API keys
+var key = require("./key");
+
+// Import the axios npm package.
 var axios = require("axios");
-const keys = require("./key.js");
-const Spotify = require('node-spotify-api');
-const spotify = new Spotify(keys.spotify);
 
+// Import the moment npm package.
+var moment = require("moment");
 
+// Import the FS package for read/write.
+var fs = require("fs");
 
+// Initialize the spotify API client using our client id and secret
+var spotify = new Spotify(key.spotify);
 
-var command=process.argv[2]
-var userInput=process.argv.slice(3).join(" ")
-console.log(userInput)
-console.log(command)
+// FUNCTIONS
+// =====================================
 
-if(command==="spotify-this-song"){
-  spotifyFunction()
-}
-if(command==="concert-this"){
-  bandsFunction()
-}
-if(command==="movie-this"){
-  movieFunction()
-}
-if(command==="do-what-it-says"){
-  readFunction()
-}
-
-
-
-
-function spotifyFunction(){
-if(!userInput){
-  userInput = "Ace of Base"
-
-
+// Helper function that gets the artist name
+var getArtistNames = function(artist) {
+  return artist.name;
 };
 
-  spotify.search({ type: 'track', query: userInput })
-  .then(function(data)
-   {
-  
-    var data = data.tracks.items
+// Function for running a Spotify search
+var getMeSpotify = function(songName) {
+  if (songName === undefined) {
+    songName = "What's my age again";
+  }
 
-    console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    console.log("The artist of this song is: " + data[0].artists[0].name);
-    console.log("The title of this song is: " + data[0].name);
-    console.log("Give it a listen: " + data[0].external_urls.spotify);
-    console.log("Its from the album: " +data[0].album.name);          
-  })
-}
+  spotify.search(
+    {
+      type: "track",
+      query: songName
+    },
+    function(err, data) {
+      if (err) {
+        console.log("Error occurred: " + err);
+        return;
+      }
 
-function bandsFunction(){
+      var songs = data.tracks.items;
 
-  axios.get("https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp").then(
-  function(response) {
-    for (var i = 0; i<response.data.length; i++) {
-      console.log("======================================================================")
-      console.log("The name of the venue is: " + response.data[i].venue.name);
-      console.log("The location of the venue is: " + response.data[i].venue.city + ', ' + response.data[i].venue.region + ', ' + response.data[i].venue.country);
-      console.log("The concert will be held on: " + (response.data[i].datetime))
-      
+      for (var i = 0; i < songs.length; i++) {
+        console.log(i);
+        console.log("artist(s): " + songs[i].artists.map(getArtistNames));
+        console.log("song name: " + songs[i].name);
+        console.log("preview song: " + songs[i].preview_url);
+        console.log("album: " + songs[i].album.name);
+        console.log("-----------------------------------");
+      }
+    }
+  );
+};
 
-       }
-  })
-}
+var getMyBands = function(artist) {
+  var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 
-function movieFunction(){
-  if(!userInput){
-    userInput = "Mr.Nobody" ;
-    console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
-    console.log("It's on Netflix!")
-  
-  
-  };
-  
+  axios.get(queryURL).then(
+    function(response) {
+      var jsonData = response.data;
 
-  axios.get("http://www.omdbapi.com/?apikey=trilogy&t="+userInput)
-  .then(
-  function(response) {
-    console.log("======================================================================")
-    console.log("The title of the movie is: " +response.data.Title)
-    console.log("This movie was released in: " +response.data.Year)
-    console.log("The IMDB rating for this movie is: " +response.data.imdbRating)
-    console.log("The Rotten Tomatoes rating for this movie is: " +response.data.Ratings[1].Value)
-    console.log("The Country in which this movie was produced is: " +response.data.Country)
-    console.log("The Language of this movie is: " +response.data.Language)
-    console.log("The Plot of this movie is: " +response.data.Plot)
-    console.log("The actors in this movie are " +response.data.Actors)
+      if (!jsonData.length) {
+        console.log("No results found for " + artist);
+        return;
+      }
 
+      console.log("Upcoming concerts for " + artist + ":");
+
+      for (var i = 0; i < jsonData.length; i++) {
+        var show = jsonData[i];
+
+        // Print data about each concert
+        // If a concert doesn't have a region, display the country instead
+        // Use moment to format the date
+        console.log(
+          show.venue.city +
+            "," +
+            (show.venue.region || show.venue.country) +
+            " at " +
+            show.venue.name +
+            " " +
+            moment(show.datetime).format("MM/DD/YYYY")
+        );
+      }
+    }
+  );
+};
+
+// Function for running a Movie Search
+var getMeMovie = function(movieName) {
+  if (movieName === undefined) {
+    movieName = "Mr Nobody";
+  }
+
+  var urlHit =
+    "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&apikey=trilogy";
+
+  axios.get(urlHit).then(
+    function(response) {
+      var jsonData = response.data;
+
+      console.log("Title: " + jsonData.Title);
+      console.log("Year: " + jsonData.Year);
+      console.log("Rated: " + jsonData.Rated);
+      console.log("IMDB Rating: " + jsonData.imdbRating);
+      console.log("Country: " + jsonData.Country);
+      console.log("Language: " + jsonData.Language);
+      console.log("Plot: " + jsonData.Plot);
+      console.log("Actors: " + jsonData.Actors);
+      console.log("Rotten Tomatoes Rating: " + jsonData.Ratings[1].Value);
+    }
+  );
+};
+
+// Function for running a command based on text file
+var doWhatItSays = function() {
+  fs.readFile("random.txt", "utf8", function(error, data) {
+    console.log(data);
+
+    var dataArr = data.split(",");
+
+    if (dataArr.length === 2) {
+      pick(dataArr[0], dataArr[1]);
+    } else if (dataArr.length === 1) {
+      pick(dataArr[0]);
+    }
   });
-}
+};
 
-function readFunction(){
-  fs.readFile('random.txt', 'utf8', function (error, data) {
-    console.log(data)
-    
-})
+// Function for determining which command is executed
+var pick = function(caseData, functionData) {
+  switch (caseData) {
+  case "concert-this":
+    getMyBands(functionData);
+    break;
+  case "spotify-this-song":
+    getMeSpotify(functionData);
+    break;
+  case "movie-this":
+    getMeMovie(functionData);
+    break;
+  case "do-what-it-says":
+    doWhatItSays();
+    break;
+  default:
+    console.log("LIRI doesn't know that");
+  }
+};
 
-}
+// Function which takes in command line arguments and executes correct function accordingly
+var runThis = function(argOne, argTwo) {
+  pick(argOne, argTwo);
+};
 
-
-
-
+// MAIN PROCESS
+// =====================================
+runThis(process.argv[2], process.argv.slice(3).join(" "));
